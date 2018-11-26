bad1ca7e703f4cc24656a7214393ce9df046849e
code-witch <minismo@sbcglobal.net>
2018-11-26 08:31:44 -0700

    No need for Money

--- src/Money.java
+++
@@ -1,4 +0,0 @@
-
-public class Money {
-	int one,five,ten,twenty,fifty,hundred,fiveHundred;
-}
--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,6 @@
 
 public class Player {
 
-	Money personalBalance = new Money();
-	int totalMoney;
+	int money;
 
 }
