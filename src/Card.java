983b9e28058353fefbb735c20e103efee24e761f
Brooke <Brooke@DESKTOP-CI6O0IM>
2018-11-26 18:40:00 -0700

    Updated

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,15 @@
+import enumeration.CardCategory;
+import enumeration.CardType;
 
 public class Card {
 	String name;
+
+	public Card(String name, String description, CardType cardType, CardCategory cardName, int cost) {
+
+	}
+
+	public Card(String name, String description, CardType cardType, CardCategory cardName) {
+
+	}
+
 }
