efbcf956ae1edb635ff063fa07f474ecaa2a5fbf
Searjasub Lopez <searjasub.lopez@outlook.com>
2018-11-29 09:08:54 -0700

    sort of finished the die class and added documentation

--- src/game/Die.java
+++ src/game/Die.java
@@ -1,19 +1,45 @@
 package game;
 
-
 public class Die {
-	
-	private int diceOne = 0;
-	private int diceTwo = 0;
-	private int total = 0;
-	
-	public int roll() {
-		
-		
-		
-		
-		total = diceOne + diceTwo;
-		return total;
+
+	private int dieOne;
+	private int dieTwo;
+
+	/**
+	 * Constructor that rolls the dice. Calls the roll() method to roll the dice
+	 */
+	public Die() {
+		roll();
+	}
+
+	/**
+	 * Rolls the dice by setting each of the dice to be a random number between 1
+	 * and 6
+	 */
+	public void roll() {
+		dieOne = (int) (Math.random() * 6) + 1;
+		dieTwo = (int) (Math.random() * 6) + 1;
+	}
+
+	/**
+	 * @return the number showing on the first die
+	 */
+	public int getDieOne() {
+		return dieOne;
+	}
+
+	/*
+	 * @return the number showing on the second die
+	 */
+	public int getDieTwo() {
+		return dieTwo;
+	}
+
+	/**
+	 * @return the total value of dieOne + dieTwo
+	 */
+	public int getTotal() {
+		return dieOne + dieTwo;
 	}
 
 }
