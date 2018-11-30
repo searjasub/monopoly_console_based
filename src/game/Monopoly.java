5b21ed790212b5216c2f5b1d53d94d95a837b13a
Beck <bes6896@yahoo.com>
2018-11-30 10:09:23 -0700

    Additions and Corrections

--- src/game/Monopoly.java
+++ src/game/Monopoly.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Monopoly {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		Game game = new Game();
 		game.run();
