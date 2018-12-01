b86ac43d89f4fc2b0f0106fc657937a22c2c5591
code-witch <minismo@sbcglobal.net>
2018-12-01 13:48:49 -0700

    notes

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -14,49 +14,67 @@ public class Demo{
 
 
     public void Go(){
-
+        // collect go
     }
 
     public void buyingProperty(){
-
+        // buying property
+        // removes money 
     }
 
     public void landOnRent(){
-
+        // paying rent
+        // checking for full set
     }
 
     public void chanceAndCommunityChest(){
-
+        // follow what the card category says
     }
 
     public void taxes(){
-
+        // pay 75$ tax
+        // ask to choose 200 or calculate 10% of net worth
     }
 
     public void jail(){
+        // 3 doubles
+        // land on square
+        // community chest
 
+        // try three times
+        // have to pay after 3 times
+        // or buy out before then
     }
 
     public void housesBuyAndSell(){
-
+        // do it evenly
+        // need a full color group
+        // sell evenly
     }
 
     public void auctioning(){
-
+        // :(
     }
 
     public void sellingAndTradingProperties(){
-
+        // with other players
     }
 
     public void mortgages(){
-
+        // do that thing
+        // 10% when received if not immediate
+        // 10% plus the mortagage
     }
 
     public void bankruptcy(){
-
+    // make sure player cant play anymore
+    // if to the bank must be auctioned
+    // un mortgaged by bank
     }
 
+    public void buyingBuildings(){
+        // do so evenly
+    }
 
 
     // Go
