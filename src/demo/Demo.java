52e83b5a5b63242eb7f22349e4ba116cc985720a
code-witch <minismo@sbcglobal.net>
2018-12-01 12:44:29 -0700

    fixes

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,16 +1,13 @@
 package demo;
 
-import org.graalvm.compiler.printer.NoDeadCodeVerifyHandler_OptionDescriptors;
-
 import game.Game;
-import game.Monopoly;
 
 public class Demo{
 
 
     public Demo(){}
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         Game game = new Game();
         game.run();
     }
@@ -55,7 +52,7 @@ public class Demo{
     public void mortgages(){
 
     }
-    
+
     public void bankruptcy(){
 
     }
