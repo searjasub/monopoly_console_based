59e5273656c54461b3636a92b21031f5ef8d8e95
Searjasub Lopez <searjasub.lopez@outlook.com>
2018-12-09 12:45:07 -0700

    Fixed showProperties(). Now if you land on utilities or railroad it will actually print their specific rents.

--- src/game/Die.java
+++ src/game/Die.java
@@ -19,12 +19,12 @@ public class Die {
 	 * and 6
 	 */
 	public void roll() {
-		Random random = new Random();
-		dieOne = random.nextInt(6) + 1;
-		dieTwo = random.nextInt(6) + 1;
+//		Random random = new Random();
+//		dieOne = random.nextInt(6) + 1;
+//		dieTwo = random.nextInt(6) + 1;
 		
-//		dieOne = 5;
-//		dieTwo = 0;
+		dieOne = 3;
+		dieTwo = 0;
 	}
 
 	/**
